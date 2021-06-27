4cadd8c16b99e86ebb6b79cedc4351c629f386c0
Sachin Verma <sachinverma53121@gmail.com>
2021-06-28 01:07:52 +0530

    db uri

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/book_rent_mern";
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(
-    MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("DB Connected!"))
   .catch((err) => console.log(`DB Error: `, err));
 
@@ -31,6 +28,5 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 
 app.listen(PORT, () => console.log(`Backend running at port ${PORT}`));
