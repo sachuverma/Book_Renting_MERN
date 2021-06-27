115bea5202d035ee2dcb8fa68cd4f6f70ac931da
Sachin Verma <sachinverma53121@gmail.com>
2021-06-27 23:55:09 +0530

    homepage and scroll on click changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -16,60 +16,54 @@ const HomePage = () => {
     <Wrapper>
       <Banner image={book1} title="Home Page" content={Content} />
       <br />
-      <h1>LoremIpsum</h1> <hr />
+      <h1>Tricks That Will Help You Read More</h1> <hr />
       <Row className="row2">
         <Col xs={5} className="window1">
           <img src={window} alt="window" />
         </Col>
         <Col xs={7} className="window2">
-          <h2>STEPS</h2>
-          <hr />
           <ul>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
-            </li>
-            <li>
-              architecto? Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit.
-            </li>
-            <li>
-              Quasi ipsum modi alias? Lorem ipsum dolor sit amet consectetur,
-            </li>
-            <li>
-              adipisicing elit. Lorem ipsum dolor sit amet consectetur,
-              adipisicing
-            </li>
-            <li>
-              elit. Quasi ipsum modi alias? Lorem ipsum dolor sit amet
-              consectetur,
-            </li>
-            <li>
-              ipsum modi alias? Lorem ipsum dolor sit amet, ipsum modi alias?
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
-            </li>
-            <li>
-              architecto? Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit.
-            </li>
-            <li>
-              Quasi ipsum modi alias? Lorem ipsum dolor sit amet consectetur,
-            </li>
-            <li>
-              adipisicing elit. Lorem ipsum dolor sit amet consectetur,
-              adipisicing
-            </li>
-            <li>
-              elit. Quasi ipsum modi alias? Lorem ipsum dolor sit amet
-              consectetur,
-            </li>
-            <li>
-              ipsum modi alias? Lorem ipsum dolor sit amet, ipsum modi alias?
-            </li>
-            <li>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
-            </li>
+            <li>Carry your book(s) everywhere.</li>
+            <p>
+              There's always an opportunity to read. Maybe at the DMV or before
+              a business meeting. Whenever you have a moment, you can break out
+              your book and read. Especially if you like to read digital
+              versions. I still prefer hard copy.
+            </p>
+
+            <li>Keep a list.</li>
+            <p>
+              There’s a sense of fulfillment and motivation tracking the books
+              you’ve read. You’ll always be able to reference the list or
+              provide recommendations, and will be inspired by the compounding
+              effect reading has.
+            </p>
+
+            <li>Listen to books.</li>
+            <p>
+              This might fall into the reading loophole, but my buddy practices
+              this strategy all of the time. He commutes about 40 minutes in the
+              morning and 40 minutes at night. He has a subscription to Audible,
+              and listens to books online. He puts the speed up on the readings
+              too, to listen (“read”) even faster.{" "}
+            </p>
+
+            <li>Prioritize.</li>
+            <p>
+              If you want to read more, reading has to become a priority. It has
+              to take precedence over watching Netflix, scrolling through
+              Instagram or making snaps. We all have the same 24 hours. Some of
+              us just know how to maximize every second.{" "}
+            </p>
+
+            <li>Read in sprints.</li>
+            <p>
+              Some days my attention span is less than others. During these
+              days, I set 20-minute timers and read in 20-minute sprints. This
+              helps me not get too burnt out, and allows me to feed my wandering
+              mind after a dedicated sprint (where I may spend five to 10
+              minutes doing something else).{" "}
+            </p>
           </ul>
         </Col>
       </Row>
@@ -106,7 +100,7 @@ const Wrapper = styled.div`
     }
   }
   .window2 {
-    padding: 7vw;
+    padding: 2vw 7vw;
     height: 100%;
     overflow: hidden;
     text-align: left;
