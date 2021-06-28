0b827f2d3db27824c5ac5fa43c55a04574420ae3
Sachin Verma <sachinverma53121@gmail.com>
2021-06-28 09:38:36 +0530

    some server.js changes

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express"),
   mongoose = require("mongoose"),
+  morgan = require("morgan"),
   cors = require("cors"),
-  app = express();
+  bodyParser = require("body-parser");
+app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/book_rent_mern";
+  "mongodb+srv://sachu:sachu1596@cluster0.b7sez.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("DB Connected!"))
@@ -22,9 +27,11 @@ app.use("/api/books", require("./routes/api/books"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api", require("./routes/api/auth"));
 
-// SERVE STATIC ASSETS IF IN PRODUCTION
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
