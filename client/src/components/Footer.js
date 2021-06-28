89f56eb1dd8f8121a97a8f658e386612e79d144a
Sachin Verma <sachinverma53121@gmail.com>
2021-06-28 13:00:41 +0530

    footer edit

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { FiFacebook, FiInstagram, FiLinkedin, FiMail } from "react-icons/fi";
 import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <Card bg="light" text="dark" className="text-center shadow-sm translucent">
       <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
+        <Card.Title className="display-5" style={{ fontSize: "1.5rem" }}>
+          <Link
+            to="/"
+            style={{ color: "black" }}
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            Book House
+          </Link>{" "}
+          |{" "}
+          <Link>
+            <FiFacebook />
+          </Link>
+          <Link>
+            <FiInstagram />
+          </Link>{" "}
+          <Link>
+            <FiLinkedin />
+          </Link>{" "}
+          <Link>
+            <FiMail />
+          </Link>
+        </Card.Title>
         <Card.Text>
           With supporting text below as a natural lead-in to additional content.
         </Card.Text>
